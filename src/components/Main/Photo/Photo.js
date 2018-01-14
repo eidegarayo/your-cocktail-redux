dca159ed5c21114e51019b2d0c073a8c607104a2
eidegarayo <eva_mig@yahoo.es>
2018-01-14 20:08:07 +0100

    show cocktail ingredients

--- src/components/Main/Photo/Photo.js
+++ src/components/Main/Photo/Photo.js
@@ -1,17 +1,29 @@
 import React from 'react'
 
+import { cocktailIngrs } from '../../../utils/utils'
+
 import './Photo.css'
 
-const Photo = (props) => {
+const Photo = props => {
   if (props.cocktail[0]) {
     const cocktail = props.cocktail[0]
+    const ingredients = cocktailIngrs(cocktail)
     return (
       <div className='cocktail-detail'>
         <img className='cocktail-img img-fluid' src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
-        <div className='cocktail-description'>
-          <h3>{cocktail.strDrink}</h3>
-          <hr />
-          <p>{cocktail.strInstructions}</p>
+        <div className='cocktail-desc'>
+          <div className='cocktail-desc-text mb-5'>
+            <h3>{cocktail.strDrink}</h3>
+            <hr />
+            <p>{cocktail.strInstructions}</p>
+          </div>
+        </div>
+        <div className='cocktail-ingrs'>
+          <ul>
+            {
+              ingredients.map((ingr, i) => <li key={i}>{ingr}</li>)
+            }
+          </ul>
         </div>
       </div>
     )
@@ -19,10 +31,12 @@ const Photo = (props) => {
     return (
       <div className='cocktail-detail'>
         <img className='cocktail-img img-fluid' src='http://www.thecocktaildb.com/images/media/drink/2t9r6w1504374811.jpg' alt='cocktail' />
-        <div className='cocktail-description'>
-          <h3>YOUR COCKTAIL</h3>
-          <hr />
-          <p className='lead'>Find the moment</p>
+        <div className='cocktail-desc'>
+          <div className='cocktail-desc-text mb-5'>
+            <h3>YOUR COCKTAIL</h3>
+            <hr />
+            <p className='lead'>Find the moment</p>
+          </div>
         </div>
       </div>
     )
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -26,3 +26,15 @@ export function sortCatList (a, b) {
   if (a.strCategory < b.strCategory) return -1
   return 0
 }
+
+export function cocktailIngrs (cocktail) {
+  let ingredients = []
+  for (let i = 1; i < 16; i++) {
+    if (cocktail['strIngredient' + i] && cocktail['strMeasure' + i]) {
+      ingredients.push(cocktail['strIngredient' + i] + ' - ' + cocktail['strMeasure' + i])
+    } else if (cocktail['strIngredient' + i]) {
+      ingredients.push(cocktail['strIngredient' + i])
+    }
+  }
+  return ingredients
+}
